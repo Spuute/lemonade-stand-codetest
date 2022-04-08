65674773728beefd9d7d73598721793fa57b1578
Patric Bergkvist <70053524+Spuute@users.noreply.github.com>
2022-04-08 22:46:24 +0200

    chore(infrastructure): adds dbcontext

---
+++ source/Lemonade.Stand.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Lemonade.Stand.Application.Interfaces.Data;
+using Lemonade.Stand.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lemonade.Stand.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<Fruit> Fruits { get; set; }
+    }
+}

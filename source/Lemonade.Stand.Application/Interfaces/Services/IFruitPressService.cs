b2d6cde08b2873c2802de74b05d7a769049b0430
Patric Bergkvist <70053524+Spuute@users.noreply.github.com>
2022-04-08 22:45:22 +0200

    chore(application): adds frutpress service interface

---
+++ source/Lemonade.Stand.Application/Interfaces/Services/IFruitPressService.cs
@@ -0,0 +1,6 @@
+namespace Lemonade.Stand.Application.Interfaces.Services
+{
+    public interface IFruitPressService {
+        //  FruitPressResult Produce(IRecipe recipe, Collection<IFruit> fruits, int moneyPaid, int orderedGlassQuantity);
+    }
+}

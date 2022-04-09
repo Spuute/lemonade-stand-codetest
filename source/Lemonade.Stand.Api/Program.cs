b09939d4dc1c74dcc6473b73e46e8f3c87731940
Patric Bergkvist <70053524+Spuute@users.noreply.github.com>
2022-04-09 19:24:33 +0200

    refactor(api): cleanup

--- source/Lemonade.Stand.Api/Program.cs
+++ source/Lemonade.Stand.Api/Program.cs
@@ -1,4 +1,8 @@
+using Lemonade.Stand.Application.Interfaces.Repositories;
 using Lemonade.Stand.Infrastructure;
+using Lemonade.Stand.Infrastructure.Persistence;
+using Lemonade.Stand.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +12,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// builder.Services.AddDbContext<ApplicationDbContext>(opts => opts.UseMySql(builder.Configuration.GetConnectionString("Db"), new MariaDbServerVersion(new System.Version(10, 5, 6))));
 builder.Services.AddInfrastructure(builder.Configuration);
+
 
 var app = builder.Build();
 

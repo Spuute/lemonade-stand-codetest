da9c838da86ec4470b4970956d8649b4a8cd5e85
Patric Bergkvist <70053524+Spuute@users.noreply.github.com>
2022-04-09 20:42:20 +0200

    chore(application): adds method to IFruitRepository

--- source/Lemonade.Stand.Application/Interfaces/Repositories/IFruitRepository.cs
+++ source/Lemonade.Stand.Application/Interfaces/Repositories/IFruitRepository.cs
@@ -5,6 +5,7 @@ namespace Lemonade.Stand.Application.Interfaces.Repositories
     public interface IFruitRepository
     {
          Task Insert(Fruit entity);
+         Task<IEnumerable<Fruit>> GetAll();
          Task SaveAsync();
     }
 }

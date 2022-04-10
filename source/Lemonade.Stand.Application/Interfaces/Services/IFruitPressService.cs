ac21e4956fd49d00e7f06a4d7af7fc9a654163f5
Patric Bergkvist <70053524+Spuute@users.noreply.github.com>
2022-04-10 18:35:18 +0200

    refactor(application): Uncomment interface

--- source/Lemonade.Stand.Application/Interfaces/Services/IFruitPressService.cs
+++ source/Lemonade.Stand.Application/Interfaces/Services/IFruitPressService.cs
@@ -1,6 +1,10 @@
+using System.Collections.ObjectModel;
+using Lemonade.Stand.Application.Models;
+using Lemonade.Stand.Core.Interfaces.Entities;
+
 namespace Lemonade.Stand.Application.Interfaces.Services
 {
     public interface IFruitPressService {
-        //  FruitPressResult Produce(IRecipe recipe, Collection<IFruit> fruits, int moneyPaid, int orderedGlassQuantity);
+        FruitPressResult Produce(IRecipe recipe, Collection<IFruit> fruits, int moneyPaid, int orderedGlassQuantity);
     }
 }

6a89bdaa56388ba8f3e05ed61673a1f8eac82533
Patric Bergkvist <70053524+Spuute@users.noreply.github.com>
2022-04-11 18:36:30 +0200

    refactor(core): change implementation to match new interface

--- source/Lemonade.Stand.Core/Entities/Recipe.cs
+++ source/Lemonade.Stand.Core/Entities/Recipe.cs
@@ -5,7 +5,7 @@ namespace Lemonade.Stand.Core.Entities
     public class Recipe : IRecipe {
         public int Id { get; set; }
         public string Name { get; set; }
-        public Fruit AllowedFruit { get; set; }
+        public Type AllowedFruit { get; set; }
         public decimal ConsumptionPerGlass { get; set; }
         public int PricePerGlass { get; set; }
     }

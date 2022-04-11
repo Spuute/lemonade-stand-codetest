7f0eb7c2ba6815645166c910bc026d7e7a297ca5
Patric Bergkvist <70053524+Spuute@users.noreply.github.com>
2022-04-11 18:37:23 +0200

    refactor(core): removes id property from Recipe

--- source/Lemonade.Stand.Core/Entities/Recipe.cs
+++ source/Lemonade.Stand.Core/Entities/Recipe.cs
@@ -3,7 +3,6 @@ using Lemonade.Stand.Core.Interfaces.Entities;
 namespace Lemonade.Stand.Core.Entities 
 {
     public class Recipe : IRecipe {
-        public int Id { get; set; }
         public string Name { get; set; }
         public Type AllowedFruit { get; set; }
         public decimal ConsumptionPerGlass { get; set; }

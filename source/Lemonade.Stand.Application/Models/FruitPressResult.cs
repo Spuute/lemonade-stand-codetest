44da0f2ca58073b5532b83e680d820091734625e
Patric Bergkvist <70053524+Spuute@users.noreply.github.com>
2022-04-12 06:52:55 +0200

    chore(application): adds properties to FruitPressResult

--- source/Lemonade.Stand.Application/Models/FruitPressResult.cs
+++ source/Lemonade.Stand.Application/Models/FruitPressResult.cs
@@ -2,6 +2,8 @@ namespace Lemonade.Stand.Application.Models
 {
     public class FruitPressResult
     {
-        
+        public int MoneyPaid { get; set; }
+        public int DeliveredGlasses { get; set; }
+        public string Beverage { get; set; }
     }
 }

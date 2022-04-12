960b129f450a167f1ddaab6c0d76b12614ec4e93
Patric Bergkvist <70053524+Spuute@users.noreply.github.com>
2022-04-12 08:36:23 +0200

    chore(webui): adds properties to indexmodel

---
+++ source/Lemonade.Stand.WebUI/Pages/Index.razor.cs
@@ -0,0 +1,21 @@
+using System.Collections.ObjectModel;
+using Lemonade.Stand.Application.Interfaces.Services;
+using Lemonade.Stand.Core.Entities;
+using Lemonade.Stand.Core.Interfaces.Entities;
+using Microsoft.AspNetCore.Components;
+
+namespace Lemonade.Stand.WebUI.Pages
+{
+    public partial class Index : ComponentBase
+    {
+        public Collection<IFruit> Fruits { get; set; }
+        public IRecipe Recipe { get; set; }
+        public Recipe<Apple> AppleLemonade { get; set; } = new Recipe<Apple>("Apple Lemonade", 2.5M, 10);
+        public Recipe<Melon> MelonLemonade { get; set; } = new Recipe<Melon>("Melon Lemonade", 0.5M, 12);
+        public Recipe<Orange> OrangeLemonade { get; set; } = new Recipe<Orange>("Orange Lemonade", 1, 9);
+        public int MoneyPaid { get; set; } = 10;
+        public int OrderedGlasses { get; set; } = 0;
+
+
+    }
+}

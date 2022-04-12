cbeaf3ecb8c2de75127f4ef12c240ccdeda48115
Patric Bergkvist <70053524+Spuute@users.noreply.github.com>
2022-04-12 23:52:22 +0200

    chore(webui): adds logic for indexpage

--- source/Lemonade.Stand.WebUI/Pages/Index.razor.cs
+++ source/Lemonade.Stand.WebUI/Pages/Index.razor.cs
@@ -1,24 +1,78 @@
 using System.Collections.ObjectModel;
-using Lemonade.Stand.Application.Interfaces.Services;
+using System.ComponentModel.DataAnnotations;
+using Lemonade.Stand.Application.Models;
 using Lemonade.Stand.Core.Entities;
 using Lemonade.Stand.Core.Interfaces.Entities;
 using Microsoft.AspNetCore.Components;
+
 
 namespace Lemonade.Stand.WebUI.Pages
 {
     public partial class Index : ComponentBase
     {
-        public Collection<IFruit> Fruits { get; set; }
-        public Collection<IRecipe> Recipes { get; set; } = new Collection<IRecipe>();
-        public IRecipe Recipe { get; set; }
+        public Collection<IFruit> Fruits { get; set; } 
+        public Collection<IRecipe> Recipes { get; set; }
         public Recipe<Apple> AppleLemonade { get; set; } = new Recipe<Apple>("Apple Lemonade", 2.5M, 10);
         public Recipe<Melon> MelonLemonade { get; set; } = new Recipe<Melon>("Melon Lemonade", 0.5M, 12);
         public Recipe<Orange> OrangeLemonade { get; set; } = new Recipe<Orange>("Orange Lemonade", 1, 9);
-        public int MoneyPaid { get; set; } = 10;
+
+        [Required]
+        [Range(10,1000, ErrorMessage = "Betala mer snåljävel")]
+        public int MoneyPaid { get; set; } 
         public int OrderedGlasses { get; set; } = 0;
+        public int FruitsAdded { get; set; } = 0;
+        public IRecipe SelectedRecipe { get; set; }
+        public bool ShowApple { get; set; } 
+        public bool ShowOrange { get; set; }
+        public bool ShowMelon { get; set; } 
+
+
+        private void InvokedFruitPressService() {
+            var apple = new Apple();
+            var orange = new Orange();
+            var melon = new Melon();
+            
+            for(int i = 0; i < FruitsAdded; i++) {
+                if(SelectedRecipe.AllowedFruit == typeof(Apple)) {
+                    Fruits.Add(apple);
+                } 
+                else if(SelectedRecipe.AllowedFruit == typeof(Orange)) {
+                    Fruits.Add(orange);
+                }
+                else {
+                    Fruits.Add(melon);
+                }
+            }
+            FruitPressResult receipt = _fruitPressService.Produce(SelectedRecipe, Fruits ,MoneyPaid, OrderedGlasses);
+            System.Console.WriteLine(Fruits.Count());
+            NavManager.NavigateTo("/receipt");
+        }
+        private void TestOnChange(ChangeEventArgs e){
+                int recipeNumber = int.Parse(e.Value.ToString());
+                SelectedRecipe = Recipes[recipeNumber];
+
+                if(SelectedRecipe.AllowedFruit == typeof(Apple)) {
+                    ShowApple = true;
+                    ShowOrange = false;
+                    ShowMelon = false;
+                } 
+                else if(SelectedRecipe.AllowedFruit == typeof(Orange)) {
+                    ShowApple = false;
+                    ShowOrange = true;
+                    ShowMelon = false;
+                }
+                else {
+                    ShowApple = false;
+                    ShowOrange = false;
+                    ShowMelon = true;
+                }
+
+        }
 
         protected override async Task OnInitializedAsync()
         {
+            Fruits = new Collection<IFruit>();
+            Recipes = new Collection<IRecipe>();
             Recipes.Add(AppleLemonade);
             Recipes.Add(MelonLemonade);
             Recipes.Add(OrangeLemonade);
